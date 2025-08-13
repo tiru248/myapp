04fc7e649d49f973bcbd85745bcb1364210e9625
tiru248 <jbflexotech@gmail.com>
2025-08-12 23:08:44 -0700

    Initial Render working app

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def home():
     return "✅ Render Flask App is Working!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    from flask import Flask
+    app.run(host="0.0.0.0", port=5000)
+
